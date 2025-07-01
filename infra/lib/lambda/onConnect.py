13b5e7ac720daab2d11cf0eb8206e538953d6ab0
Cyanmin <tomohiro20030220@gmail.com>
2025-07-01 19:54:31 +0900

    fix lambda

--- infra/lib/lambda/onConnect.py
+++ infra/lib/lambda/onConnect.py
@@ -1,19 +1,22 @@
 import boto3
 import os
+import uuid
 
 dynamodb = boto3.resource('dynamodb')
 player_table = dynamodb.Table(os.environ['PLAYER_TABLE'])
 
 def handler(event, context):
-    # 必要であれば、接続 ID をログ出力
     connection_id = event['requestContext']['connectionId']
 
-    # DynamoDB にプレイヤー仮登録
+    # 仮の roomId/playerId を生成（あとで joinRoom で更新される前提）
+    temp_room_id = f"pending-{str(uuid.uuid4())}"
+    temp_player_id = f"conn-{connection_id[:8]}"
+
     player_table.put_item(
         Item={
             'connectionId': connection_id,
-            'roomId' : '', # 未設定状態で仮登録
-            'playerId': '',
+            'roomId': temp_room_id,
+            'playerId': temp_player_id,
             'elapsed': None,
             'score': 0,
         }
